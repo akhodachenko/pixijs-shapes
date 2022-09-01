2967a5ef9498fd577b3918bc40d5958eae9279dc
DmytroDiachenko1989 <diachenkod@mydigicode.com>
2022-09-01 15:54:12 +0300

    Added colors of shapes

--- src/Model.js
+++ src/Model.js
@@ -5,7 +5,7 @@ export default class Model {
 	constructor() {
 		this.width = 800;
 		this.height = 600;
-		this.shapeColors = [0xFF7DFF, 0xFFFF7D, 0x5A76E5, 0x008080, 0x0048BA, 0xFFDB00, 0x46FF68, 0x5FDCFF, 0xFFBC8C];
+		this.shapeColors = [0xFF7DFF, 0xFFFF7D, 0x5A76E5, 0x008080, 0xBE7DFF, 0xFFDB00, 0x5DEA5D, 0x5FDCFF, 0xFD9AB1];
 		this.shapeTypes = ['triangle', 'quadrangle', 'pentagon', 'hexagon', 'circle', 'ellipse'];
 		this.pixels = 0;
 
